4ec8b8b262e8f4f14c490937a9f545ebd1ae1791
tiandingyi <51780848+tiandingyi@users.noreply.github.com>
2025-04-21 16:48:06 +0800

    feature: 增加部分神煞判断

--- shenshatools.py
+++ shenshatools.py
@@ -17,6 +17,10 @@ def get_shensha(sizhu, ganzhi):
     year_gan_zhi = sizhu[0]  # 获取年柱
     month_gan_zhi = sizhu[1]  # 获取月柱
     day_gan_zhi = sizhu[2]  # 获取日柱
+
+    year_kongwang = kongwangs[sizhu[0]] # 年空亡
+    day_kongwang = kongwangs[sizhu[2]] # 日空亡
+
     
     # 判断天乙贵人
     if ganzhi[1] in tianyis[year_gan_zhi[0]]:
@@ -75,6 +79,154 @@ def get_shensha(sizhu, ganzhi):
         result.append("年红艳煞")
     if ganzhi[1] in hongyans[day_gan_zhi[0]]:
         result.append("日红艳煞")
+    
+    # 判断将星
+    if ganzhi[1] in jiangxings[year_gan_zhi[1]]:
+        result.append("年将星")
+    if ganzhi[1] in jiangxings[day_gan_zhi[1]]:
+        result.append("日将星")
 
-        
+    # 判断华盖
+    if ganzhi[1] in huagais[year_gan_zhi[1]]:
+        result.append("年华盖")
+    if ganzhi[1] in huagais[day_gan_zhi[1]]:
+        result.append("日华盖")
+
+    # 判断驿马
+    if ganzhi[1] in yimas[year_gan_zhi[1]]:
+        result.append("年驿马")
+    if ganzhi[1] in yimas[day_gan_zhi[1]]:
+        result.append("日驿马")
+
+    # 判断劫煞
+    if ganzhi[1] in jieshas[year_gan_zhi[1]]:
+        result.append("年劫煞")
+    if ganzhi[1] in jieshas[day_gan_zhi[1]]:
+        result.append("日劫煞")
+
+    # 判断亡神
+    if ganzhi[1] in wangshens[year_gan_zhi[1]]:
+        result.append("年亡神")
+    if ganzhi[1] in wangshens[day_gan_zhi[1]]:
+        result.append("日亡神")
+
+    # 判断桃花
+    if ganzhi[1] in taohuas[year_gan_zhi[1]]:
+        result.append("年桃花")
+    if ganzhi[1] in taohuas[day_gan_zhi[1]]:
+        result.append("日桃花")
+
+    # 判断天厨贵人
+    if ganzhi[1] in tianchus[month_gan_zhi[0]]:
+        result.append("年天厨贵人")
+    
+    # 判断天医
+    if ganzhi[1] in tianyi[month_gan_zhi[1]]:
+        result.append("月天医")
+
+    # 判断卦气
+    if ganzhi[1] in guaqis[year_gan_zhi[0]]:
+        result.append("年卦气")
+    if ganzhi[1] in guaqis[day_gan_zhi[0]]:
+        result.append("日卦气")
+    
+    # 判断祿勋
+    if ganzhi[1] in luxuns[year_gan_zhi[0]]:
+        result.append("年祿勋")
+    if ganzhi[1] in luxuns[day_gan_zhi[0]]:
+        result.append("日祿勋")
+
+    # 判断国印
+    if ganzhi[1] in guoyins[year_gan_zhi[0]]:
+        result.append("年国印")
+    if ganzhi[1] in guoyins[day_gan_zhi[0]]:
+        result.append("日国印")
+    
+    # 判断空亡
+    if ganzhi[1] in year_kongwang:
+        result.append("年空亡")
+    if ganzhi[1] in day_kongwang:
+        result.append("日空亡")
+
+    # 判断阴差阳错
+    if ganzhi in yinchayangcuo_list:
+        result.append("阴差阳错")
+
+    # 判断金舆
+    if ganzhi[1] in jin_yu_dict[year_gan_zhi[0]]:
+        result.append("年金舆")
+    if ganzhi[1] in jin_yu_dict[day_gan_zhi[0]]:
+        result.append("日金舆")
+
+    # 红鸾
+    if ganzhi[1] in hongluan_dict[year_gan_zhi[1]]:
+        result.append("年红鸾")
+    if ganzhi[1] in hongluan_dict[day_gan_zhi[1]]:
+        result.append("日红鸾")
+
+    # 天喜
+    if ganzhi[1] in tianxi_dict[year_gan_zhi[1]]:
+        result.append("年天喜")
+    if ganzhi[1] in tianxi_dict[day_gan_zhi[1]]:
+        result.append("日天喜")
+
+    # 丧门
+    if ganzhi[1] in sangmen_dict[year_gan_zhi[1]]:
+        result.append("年丧门")
+
+    # 吊客
+    if ganzhi[1] in diaoke_dict[year_gan_zhi[1]]:
+        result.append("年吊客")
+
+    # 大耗
+    if ganzhi[1] in dahao_dict[year_gan_zhi[1]]:
+        result.append("年大耗")
+
+    # 小耗
+    if ganzhi[1] in xiaohao_dict[year_gan_zhi[1]]:
+        result.append("年小耗")
+
+    # 病符
+    if ganzhi[1] in bingfu_dict[year_gan_zhi[1]]:
+        result.append("年病符")
+
+    # 死符
+    if ganzhi[1] in sifu_dict[year_gan_zhi[1]]:
+        result.append("年死符")
+
+    # 官符
+    if ganzhi[1] in guanfu_dict[year_gan_zhi[1]]:
+        result.append("年官符/五鬼")
+    
+    # 旌德煞
+    if ganzhi[0] in jingde_year_dict[year_gan_zhi[1]]:
+        result.append("年旌德煞")
+
+    # 旌钺煞
+    if ganzhi[1] in jingyue_dict[year_gan_zhi[1]]:
+        result.append("年旌钺煞")
+
+    # 血支
+    if ganzhi[1] in xuezhi_dict[year_gan_zhi[1]]:
+        result.append("年血支")
+
+    # 血刃
+    if ganzhi[1] in xueren_dict[month_gan_zhi[1]]:
+        result.append("月血刃")
+    
+    # 流霞
+    if ganzhi[1] in liuxia_dict[day_gan_zhi[0]]:
+        result.append("日流霞")
+
+    # 福星贵人
+    if ganzhi[1] in fuxing_dict[day_gan_zhi[0]]:
+        result.append("日福星贵人")
+
+    # 自缢煞
+    if ganzhi[1] in ziyi_dict[year_gan_zhi[1]]:
+        result.append("年自缢煞")
+    if ganzhi[1] in ziyi_dict[day_gan_zhi[1]]:
+        result.append("日自缢煞")
+    
+
     return result
